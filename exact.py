a317af24cd626103df84723677fb4705d6de28c6
Lea Hohmann <lhohmann@kth.se>
2020-03-30 17:06:13 +0200

    Edited exact calculation function further

---
+++ exact.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from itertools import permutations
+import numpy as np
+import pandas as pd
+    
+
+
+# In[ ]:
+
+
+def shortestpath(inputdataframe):
+    pathvector = np.array(inputdataframe.index)
+    permutating_vector = pathvector[:-1]
+    distance = 1000000
+    for permutating_path in permutations(permutating_vector,len(permutating_vector)):
+        path = np.append(permutating_path,pathvector[-1])
+        rearrangeddata = inputdataframe.reindex(path,axis='columns')
+        rolledpath = np.roll(path,len(path)-1)
+        rearrangeddata = rearrangeddata.reindex(rolledpath,axis='index')
+        pathlength = np.sum(np.diagonal(rearrangeddata))
+        if pathlength < distance:
+                distance = pathlength
+                shortest = path 
+    print(shortest)
+            
+
+        
+
