8fa6ae28c90d581c7a06783d81096d555e9062ba
Lea Hohmann <lhohmann@kth.se>
2020-04-03 10:47:46 +0200

    Added docstrings

--- exact.py
+++ exact.py
@@ -13,10 +13,31 @@ import pandas as pd
 # In[ ]:
 
 
-def shortestpath(inputdataframe):
+def shortestpath(inputdataframe,distance):
+    """
+    Calculates shortest path from data matrix with symmetric TSP analytically
+    
+    Finds shortest path by iterating through all possible paths and keeping the shortest one.
+    Uses the vector of indices of the data matrix as a path vector and permutates it, calculates the distance and compares to the previous value, then keeps the shorter one.
+    
+    Parameters
+    ---------
+    inpudataframe: Pandas DataFrame
+                   Passed from salesman.salesman function (or use the salesman.dataimport function to retrieve it from file)
+    distance: Float
+              Maximum possible route length, passed from salesman.salesman
+                   
+    Returns
+    ------
+    shortest: Vector of strings
+              Indices of inputdataframe in order of shortest route
+    distance: Float
+              Distance travelled by shortest route
+    
+    """
     pathvector = np.array(inputdataframe.index)
     permutating_vector = pathvector[:-1]
-    distance = 1000000
+    
     for permutating_path in permutations(permutating_vector,len(permutating_vector)):
         path = np.append(permutating_path,pathvector[-1])
         rearrangeddata = inputdataframe.reindex(path,axis='columns')
@@ -26,6 +47,7 @@ def shortestpath(inputdataframe):
         if pathlength < distance:
                 distance = pathlength
                 shortest = path 
+                
     return distance, shortest
             
 
